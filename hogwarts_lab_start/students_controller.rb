9dd504573bc4c4040afbe122f84271cc968c9af3
Ray <ray.greenan.89@outlook.com>
2018-11-07 17:50:36 +0000

    hogwarts part deux

--- hogwarts_lab_start/students_controller.rb
+++ hogwarts_lab_start/students_controller.rb
@@ -2,7 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('models/student')
 require_relative('models/house')
-also_reload('./models/*')
+also_reload('models/*')
 
 # index
 get '/students' do
@@ -22,19 +22,28 @@ get '/students/:id' do
   erb(:show)
 end
 
-
-# edit
-get 'students/edit' do
-  @student = Student.find(params[:id])
-  erb(:edit)
-end
-
 # create
   post '/students' do
     Student.new(params).save()
     redirect to 'students'
   end
 
+# edit
+get '/students/:id/edit' do
+  @student = Student.find(params[:id])
+  erb(:edit)
+end
+
 # update
+post '/students/:id' do
+  student = Student.new(params)
+  student.update
+  redirect to 'students/' + params[:id]
+end
 
 # destroy
+post '/students/:id/delete' do
+  student = Student.find(params[:id])
+  student.delete
+  redirect to 'students/'
+end
